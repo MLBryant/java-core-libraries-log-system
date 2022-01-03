6b235379d0037f5bc769655727c4d2519c2f487c
Mason Bryant <mlbryant6@gmail.com>
2022-01-03 16:11:53 -0600

    Initial commit

---
+++ 03/demos/carvedrockfitness_no_log/src/main/java/order/OrderService.java
@@ -0,0 +1,85 @@
+package order;
+
+import user.User;
+import user.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.*;
+
+public class OrderService {
+    private OrderRepository orderRepository = new OrderRepository();
+    private static final Logger LOGGER = Logger.getLogger(OrderService.class.getName());
+    static {
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        LOGGER.addHandler(consoleHandler);
+    }
+
+    //get all logic
+    public List<Order> getAllOrders() {
+        LOGGER.log(Level.INFO, "Getting all the orders");
+        return orderRepository.findAll();
+    }
+
+    //get by user logic
+    public List<Order> getAllOrdersByUser(User user) {
+        if (UserRepository.getDummyDataList().contains(user)) {
+            LOGGER.log(Level.INFO, "Getting all orders for user id: " + user.getId() + " and username: " + user.getUserName());
+            return orderRepository.findByUser(user);
+        } else {
+            try {
+                throw new Exception("User doesn't exist");
+            } catch (Exception e) {
+                LOGGER.log(Level.WARNING, "Cannot get orders for a non-existing user, username: " + user.getUserName() + " id: " + user.getId());
+                e.printStackTrace();
+            }
+            return new ArrayList<>();
+        }
+    }
+
+    //add order logic
+    public boolean addOrder(Order order) {
+        if (order.getOrderDateTime().isAfter(LocalDateTime.now())) {
+            try {
+                throw new Exception("Can't order in the future!");
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, "Placing order failed, cannot place order in the future. User: " + order.getUser().getId() + " order: " + order.getProducts());
+                e.printStackTrace();
+                return false;
+            }
+        }
+        if (order.getProducts().size() < 1) {
+            try {
+                throw new Exception("Order must consist of at least one product!");
+            } catch (Exception e) {
+                e.printStackTrace();
+                LOGGER.log(Level.WARNING, "Cannot get empty order, username: " + order.getUser().getUserName() + " id: " + order.getUser().getId());
+                return false;
+            }
+        }
+        LOGGER.log(Level.INFO, "Saving order: " + order.getProducts() + " for user: " + order.getUser().getId());
+        return orderRepository.save(order);
+    }
+
+    //delete logic
+    public boolean deleteOrder(Order order) {
+        if (order.getOrderStatus() == OrderStatus.COMPLETED) {
+            try {
+                throw new Exception("Can't cancel a completed order!");
+            } catch (Exception e) {
+                LOGGER.log(Level.WARNING, "Cannot cancel order with id: " + order.getId() + " because it is already completed");
+                e.printStackTrace();
+            }
+        } else if (order.getOrderStatus() == OrderStatus.CANCELLED) {
+            try {
+                throw new Exception("Order was already cancelled!");
+            } catch (Exception e) {
+                LOGGER.log(Level.WARNING, "Cannot cancel order with id: " + order.getId() + " because it is already cancelled");
+                e.printStackTrace();
+            }
+        }
+        return orderRepository.remove(order);
+    }
+
+}
